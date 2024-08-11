7c6454ba1500e17cd8d3ae2e14ee70aa131631b5
Aleksandar3811 <alek.kondrov@gmail.com>
2024-08-11 13:42:06 +0300

    This is the skeleton of the project

---
+++ Countries/src/test/java/bg/softUni/Countries/CountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softUni.Countries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
