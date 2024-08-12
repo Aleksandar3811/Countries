d738647248aa34a9ba25a0e9aec9d415d143672b
Aleksandar3811 <alek.kondrov@gmail.com>
2024-08-12 11:08:42 +0300

    Get add-country

--- Countries/src/main/java/bg/softUni/Countries/web/CountryController.java
+++ Countries/src/main/java/bg/softUni/Countries/web/CountryController.java
@@ -1,10 +1,13 @@
 package bg.softUni.Countries.web;
 
+import bg.softUni.Countries.entity.CategoryType;
+import bg.softUni.Countries.entity.Level;
 import bg.softUni.Countries.entity.dto.CountryShortInfoDto;
 import bg.softUni.Countries.service.CountryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -25,6 +28,16 @@ public class CountryController {
         model.addAttribute("allCountries", routes);
 
         return "countries";
+    }
+    @GetMapping("add-country")
+    public ModelAndView addRoute() {
+        ModelAndView modelAndView = new ModelAndView("add-country");
+
+        modelAndView.addObject("country", new CountryShortInfoDto());
+        modelAndView.addObject("levels", Level.values());
+        modelAndView.addObject("categoryTypes", CategoryType.values());
+
+        return modelAndView;
     }
 
 
