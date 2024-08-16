8628f4efd6ea8dccbe1ba5f31d9fe8fab7d475a7
Aleksandar3811 <alek.kondrov@gmail.com>
2024-08-16 04:40:34 +0300

    User found test Done!

--- Countries/src/test/java/bg/softUni/Countries/service/AppUserDetailsServiceTest.java
+++ Countries/src/test/java/bg/softUni/Countries/service/AppUserDetailsServiceTest.java
@@ -3,6 +3,10 @@ package bg.softUni.Countries.service;
 
 
 
+import bg.softUni.Countries.entity.Level;
+import bg.softUni.Countries.entity.Role;
+import bg.softUni.Countries.entity.User;
+import bg.softUni.Countries.entity.UsersRoles;
 import bg.softUni.Countries.repository.UserRepository;
 import bg.softUni.Countries.service.session.AppUserDetailsService;
 import org.junit.jupiter.api.Assertions;
@@ -12,8 +16,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -31,6 +42,41 @@ public class AppUserDetailsServiceTest {
     void setUp() {
         toTest = new AppUserDetailsService(mockUserRepository);
     }
+    @Test
+    void  testLoadUserByUsername_UserFound() {
+        Role role=new Role();
+        role.setName(UsersRoles.USER);
+        Role role2=new Role();
+        role2.setName(UsersRoles.ADMIN);
+
+        User testUser = new User();
+        testUser.setUsername(TEST_USERNAME);
+        testUser.setPassword("12345");
+        testUser.setFullName("zzzxxx");
+        testUser.setAge(45);
+        testUser.setEmail("zzz@abv.bg");
+        testUser.setRoles(Set.of(
+                role,role2
+        ));
+        testUser.setLevel(Level.INTERMEDIATE);
+
+
+
+        when(mockUserRepository.findByUsername(TEST_USERNAME))
+                .thenReturn(Optional.of(testUser));
+
+
+        UserDetails userDetails = toTest.loadUserByUsername(TEST_USERNAME);
+
+
+        Assertions.assertEquals(TEST_USERNAME,userDetails.getUsername());
+        Assertions.assertEquals(testUser.getPassword(),userDetails.getPassword());
+
+
+
+
+    }
+
 
 
     @Test
